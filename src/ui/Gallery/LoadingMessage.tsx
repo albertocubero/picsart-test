e2248ffcb51218a455c1593d611e44d54c531f9e
Alberto Cubero Navas <albertocuberonavas@gmail.com>
2024-12-18 15:41:33 +0100

    LoadingMessage: styled component

--- src/ui/Gallery/LoadingMessage.tsx
+++ src/ui/Gallery/LoadingMessage.tsx
@@ -1,18 +1,20 @@
 import React from "react";
+import styled from "styled-components";
+
+const LoadingMessageWrapper = styled.p`
+  position: fixed;
+  top: 0;
+  left: 50%;
+  transform: translateX(-50%);
+  font-size: 16px;
+  background-color: red;
+  color: white;
+  padding: 8px;
+  border-radius: 4px;
+`;
 
 export const LoadingMessage: React.FC = () => (
-  <p
-    data-testid="loading-message"
-    style={{
-      position: "fixed",
-      top: "0",
-      left: "50%",
-      transform: "translateX(-50%)",
-      fontSize: "16px",
-      backgroundColor: "red",
-      padding: "8px",
-    }}
-  >
+  <LoadingMessageWrapper data-testid="loading-message">
     Loading more images...
-  </p>
+  </LoadingMessageWrapper>
 );
