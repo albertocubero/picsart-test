d83ccd157e48f868fa8802358fb4a49bde14330a
Alberto Cubero Navas <albertocuberonavas@gmail.com>
2024-12-19 00:34:16 +0100

    Web Vitals: added metrics

--- src/reportWebVitals.ts
+++ src/reportWebVitals.ts
@@ -1,9 +1,11 @@
-import { onLCP, onINP, onCLS } from "web-vitals";
+import { onLCP, onINP, onCLS, onFCP, onTTFB } from "web-vitals";
 
 const logMetric = (metric: any) => {
   console.log(`[Web Vitals] ${metric.name}:`, metric.value);
 };
 
 onCLS(logMetric);
+onFCP(logMetric);
 onINP(logMetric);
 onLCP(logMetric);
+onTTFB(logMetric);
